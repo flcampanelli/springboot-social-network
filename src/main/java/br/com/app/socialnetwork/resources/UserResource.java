539287b2e88b45135f9bb618769f23347cb86fd0
flcampanelli <flcampanelli@gmail.com>
2024-05-27 20:57:15 -0300

    feat(user): add mongodb connection, user repository and service

--- src/main/java/br/com/app/socialnetwork/resources/UserResource.java
+++ src/main/java/br/com/app/socialnetwork/resources/UserResource.java
@@ -1,26 +1,25 @@
 package br.com.app.socialnetwork.resources;
 
 import br.com.app.socialnetwork.entities.User;
+import br.com.app.socialnetwork.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User endrick = new User("1", "Endrick", "endrick@gmail.com");
-        User estevao = new User("2", "Estevão", "estevao@gmail.com");
-        List<User> users = new ArrayList<>();
-        users.addAll(Arrays.asList(endrick, estevao));
+        List<User> users = userService.findAll();
         return ResponseEntity.ok().body(users);
     }
 }
---
+++ src/main/java/br/com/app/socialnetwork/services/UserService.java
@@ -0,0 +1,19 @@
+package br.com.app.socialnetwork.services;
+
+import br.com.app.socialnetwork.entities.User;
+import br.com.app.socialnetwork.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
